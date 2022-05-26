a36a792cc08828317fb93cce57d783874f5342d7
TJacka <vegozzy@gmail.com>
2022-05-25 20:07:12 -0700

    Added initial random player selection functionality

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 // Create variables for the game state
 let player1Score = 0
 let player2Score = 0
-let player1Turn = true
+
+function playerTurn() {
+    let turn = (Math.floor(Math.random() * 2));
+    if (turn) {
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        return 1;
+    } else {
+        player2Dice.classList.add("active")
+        player1Dice.classList.remove("active")
+        return 2;
+    }
+} 
 
 // Create variables to store references to the necessary DOM nodes
 const player1Dice = document.getElementById("player1Dice")
@@ -11,6 +23,8 @@ const player2Scoreboard = document.getElementById("player2Scoreboard")
 const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
+
+message.textContent = `Player ${playerTurn()} Turn`;
 
 function showResetButton() {
     rollBtn.style.display = "none"
@@ -22,7 +36,7 @@ function showResetButton() {
  rollBtn.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 6) + 1
 
-    if (player1Turn) {
+    if (playerTurn) {
         player1Score += randomNumber
         player1Scoreboard.textContent = player1Score
         player1Dice.textContent = randomNumber
@@ -45,34 +59,14 @@ function showResetButton() {
         message.textContent = "Player 2 Won 🎉"
         showResetButton()
     }
-    player1Turn = !player1Turn
+    playerTurn = !playerTurn
 })
- 
-resetBtn.addEventListener("click", function(){
-    reset()
-})
-
-function reset() {
-    player1Score = 0
-    player2Score = 0
-    player1Turn = true
-    player1Scoreboard.textContent = 0
-    player2Scoreboard.textContent = 0
-    player1Dice.textContent = "-"
-    player2Dice.textContent = "-"
-    message.textContent = "Player 1 Turn"
-    resetBtn.style.display = "none"
-    rollBtn.style.display = "block"
-    doubleBtn.style.display = "block"
-    player2Dice.classList.remove("active")
-    player1Dice.classList.add("active")
-}
 
 doubleBtn.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 6) + 1
     const doubleOrNothing = Math.floor(Math.random() * 2);
 
-    if (player1Turn) {
+    if (playerTurn) {
         player1Score += doubleOrNothing * (randomNumber * 2)
         player1Scoreboard.textContent = player1Score
         player1Dice.textContent = doubleOrNothing * (randomNumber * 2)
@@ -95,5 +89,33 @@ doubleBtn.addEventListener("click", function() {
         message.textContent = "Player 2 Won 🎉"
         showResetButton()
     }
-    player1Turn = !player1Turn
+    playerTurn = !playerTurn
 })
+
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    if (playerTurn == false) {
+        message.textContent = `Player 2 Turn`
+    } else {
+        message.textContent = `Player 1 Turn`
+    }
+    player1Score = 0
+    player2Score = 0
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    doubleBtn.style.display = "block"
+    if (playerTurn() === 1) {
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+    } else {
+        player2Dice.classList.add("active")
+        player1Dice.classList.remove("active")
+    }
+}
